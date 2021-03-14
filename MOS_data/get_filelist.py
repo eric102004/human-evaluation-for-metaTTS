b67e071ce5d4c2986dd0fe7198b48cc33a8e74fd
eric102004 <h511163@nehs.hc.edu.tw>
2021-03-14 16:45:27 +0800

    change filenames of wav files

--- MOS_data/get_filelist.py
+++ MOS_data/get_filelist.py
@@ -6,29 +6,43 @@ from sheet_file_dict import sheet_file_dict_nat, sheet_file_dict_sim
 def get_filelist_naturalness():
     '''
     filelist = []
+    sheet_file_dict_nat = dict()
     dir_list = os.listdir('.')
     for dirname in dir_list:
         if dirname.endswith('.py'):
             dir_list.remove(dirname)
     dir_list.remove('reference')
-    for dirname in dir_list:
+    dir_list.remove('__pycache__')
+    modelist1 = ['baseline_emb', 'baseline_emb_ftemb','baseline_ft','meta_emb','meta_ft']
+    modelist2 = ['real']
+    for mode in modelist1:
+        for shot in [1,5,20]:
+            dirname = f'{mode}_shot{shot}'
+            for speaker in os.listdir(dirname):
+                #filename = os.listdir(os.path.join(dirname, speaker))[0]
+                filename = f'{mode}_{shot}_{speaker}.wav'
+                filepath = os.path.join('MOS_data',dirname,speaker, filename)
+                filelist.append(filepath)
+    for mode in modelist2: 
+        dirname = mode
         for speaker in os.listdir(dirname):
-            filename = os.listdir(os.path.join(dirname, speaker))[0]
+            #filename = os.listdir(os.path.join(dirname, speaker))[0]
+            filename = f'{mode}_{speaker}.wav'
             filepath = os.path.join('MOS_data',dirname,speaker, filename)
             filelist.append(filepath)
 
     random.shuffle(filelist)
 
     for i in range(29):
-        sheet_file_dict[i] = filelist[20*i:20*(i+1)]
-    sheet_file_dict[29] = filelist[20*29:]
-    print(sheet_file_dict)
+        sheet_file_dict_nat[i] = filelist[20*i:20*(i+1)]
+    sheet_file_dict_nat[29] = filelist[20*29:]
+    print(sheet_file_dict_nat)
     '''
     for speaker in sheet_file_dict_nat.keys():
-        print(speaker, sheet_file_dict_nat[speaker])
-
+        print(speaker, len(sheet_file_dict_nat[speaker]))
+    
 def get_filelist_similarity():
-    '''
+    ''' 
     filelist = []
     sheet_file_dict_sim = dict()
     dir_list = os.listdir('.')
@@ -37,13 +51,26 @@ def get_filelist_similarity():
             dir_list.remove(dirname)
     dir_list.remove('reference')
     dir_list.remove('__pycache__')
-    for dirname in dir_list:
+    modelist1 = ['baseline_emb', 'baseline_emb_ftemb','baseline_ft','meta_emb','meta_ft']
+    modelist2 = ['real']
+    for mode in modelist1:
+        for shot in [1,5,20]:
+            dirname = f'{mode}_shot{shot}'
+            for speaker in os.listdir(dirname):
+                filename_gen = f'{mode}_{shot}_{speaker}.wav'
+                filename_ref = f'reference_{speaker}.wav'
+                filepath_gen = os.path.join('MOS_data',dirname,speaker, filename_gen)
+                filepath_ref = os.path.join('MOS_data','reference',speaker, filename_ref)
+                filelist.append((filepath_ref,filepath_gen))
+    for mode in modelist2:
+        dirname = mode
         for speaker in os.listdir(dirname):
-            filename_gen = os.listdir(os.path.join(dirname, speaker))[0]
-            filename_ref = os.listdir(os.path.join('reference', speaker))[0]
+            filename_gen = f'{mode}_{speaker}.wav'
+            filename_ref = f'reference_{speaker}.wav'
             filepath_gen = os.path.join('MOS_data',dirname,speaker, filename_gen)
             filepath_ref = os.path.join('MOS_data','reference',speaker, filename_ref)
             filelist.append((filepath_ref,filepath_gen))
+
 
     random.shuffle(filelist)
 
@@ -54,7 +81,8 @@ def get_filelist_similarity():
     '''
     for form_id in sheet_file_dict_sim.keys():
         print(form_id, len(sheet_file_dict_sim[form_id]))
-
+    
 
 if __name__ == '__main__':
+    #get_filelist_naturalness()
     get_filelist_similarity()
--- render_mos.py
+++ render_mos.py
@@ -3,31 +3,46 @@
 
 from jinja2 import FileSystemLoader, Environment
 import sys
+import os
+import argparse
 import codecs
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
+from MOS_data.sheet_file_dict import sheet_file_dict_nat
+from MOS_data.speaker_sen import speaker_sen_dict
 
-def main(form_id, num_type):
+def main(form_id):
     """Main function."""
     loader = FileSystemLoader(searchpath="./templates")
     env = Environment(loader=loader)
     template = env.get_template("mos.html.jinja2")
 
-    script = ['script1']*num_type + ['script2']*num_type + ['script3']*num_type + ['script4']*num_type
+    filelist_row = sheet_file_dict_nat[form_id]
+    filelist = [os.path.join('https://github.com/eric102004/human-evaluation-for-metaTTS/tree/master', filepath) for filepath in filelist_row ]
+    print(filelist)
+
+    '''    
+    script = []
+    for i in range(20):
+        speaker_id = filelist[i].split('/')
+        script.append(filelist[i].split('/')[-1][:-4].split('_')[-1])
+    
     html = template.render(
         page_title=f"MOS 實驗表單 {form_id}",
-        form_url="https://script.google.com/macros/s/AKfycbyOYo43xTRKdQDReFUvv4ILAmBW3P26M2Drs4wdfSRzSrayGtxPpe4kmz3KNBkeYMq13w/exec",
+        form_url="https://script.google.com/macros/s/AKfycbys0SzKRa--zMxnDAIIXEWGAnkh2HOWC5zUD9eaQphn5KvyIOMYx9Ezd08PRqv21C67/exec",
         form_id=form_id,
         questions=[
             {
                 "title": f"問題 {index}",
                 "script": script[index-1],
-                "audio_path": f"wavs/test_{form_id}_{index}.wav",
+                "audio_path": filelist[index-1],
                 "name": f"q{index}"
-            } for index in range(1,num_type*4+1)],
+            } for index in range(1,21)],
     )
     print(html)
-
-
+    '''
 if __name__ == "__main__":
-    main(1,6)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--form_id", type=int)
+    args = parser.parse_args()
+    main(args.form_id)
